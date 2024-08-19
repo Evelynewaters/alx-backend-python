dc4a49c7ae1bd27417b98658b4872ccd46d4f97b
Evelynewaters <eveclaraify@gmail.com>
2024-08-19 02:12:42 +0000

    Docs- New solution files

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+A type-annotated function sum_list which takes a list input_list of floats
+"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    Calculate the sum of a list of floats.
+
+    Parameters:
+        input_list (List[float]): A list of floats.
+
+    Returns:
+        float: The sum of the floats in the input list.
+    """
+    return sum(input_list)
---
+++ 0x00-python_variable_annotations/README.md
@@ -0,0 +1,1 @@
+This directory contains solutions files for project 0x00-python_variable_annotations
